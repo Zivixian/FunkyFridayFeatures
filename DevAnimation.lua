63cf88afdf7f59a2450655c848f6938c6c2e29c6
Zivixian <119481132+Zivixian@users.noreply.github.com>
2022-11-30 19:12:33 -0600

    Update DevAnimation.lua

--- DevAnimation.lua
+++ DevAnimation.lua
@@ -1,12 +1,12 @@
 local args = {
-        [1] = {
-            [1] = "Server",
-            [2] = "BuyItem"
-        },
-        [2] = {
-            [1] = "MoveSets",
-            [2] = "aus_si"
-        }
+    [1] = {
+        [1] = "Server",
+        [2] = "BuyItem"
+    },
+    [2] = {
+        [1] = "MoveSets",
+        [2] = "aus_si"
     }
+}
 
-    game:GetService("ReplicatedStorage").RF:InvokeServer(unpack(args))
+game:GetService("ReplicatedStorage").RF:InvokeServer(unpack(args))
